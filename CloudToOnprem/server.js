bcfe57e265ae7189a87e7613dd10d19671b6f277
Kunj Bihari Shukla <kunjshukla@hotmail.com>
2021-05-10 18:28:17 +0530

    Update PR + POList + PO changes

--- CloudToOnprem/server.js
+++ CloudToOnprem/server.js
@@ -109,6 +109,31 @@ const _callOnPrem = async function(connProxyHost, connProxyPort, connJwtToken, d
 	})    
 }
 
+// app.get('/podetails', async function (req, res) {
+//     // call destination service
+// 	const destJwtToken = await _fetchJwtToken(destSrvCred.url, destSrvCred.clientid, destSrvCred.clientsecret)
+// 	const destiConfi = await _readDestinationConfig('s14_purchase_order', destSrvCred.uri, destJwtToken)
+//     queryParam = url.parse(req.url, true).query;
+        
+//     // call onPrem system via connectivity service and Cloud Connector
+// 	const connJwtToken = await _fetchJwtToken(conSrvCred.token_service_url, conSrvCred.clientid, conSrvCred.clientsecret)
+//     try{
+//         const result =  await _poDetails(conSrvCred.onpremise_proxy_host, conSrvCred.onpremise_proxy_http_port, connJwtToken, destiConfi)
+        
+//         str = result.d.CreationDate;
+//         res0 = str.split("cf(");
+//         res1 = res0[1].split(")");   
+//         res2 = new Date(parseInt(res1[0]));
+//         result.d.CreationDate = res2;
+
+//         res.json(result)
+//     }
+//     catch(e) {
+//         console.log('Catch an error: ', e)
+//         res.json({"d":{"error": "error"}})
+//     }
+// })
+
 app.get('/podetails', async function (req, res) {
     // call destination service
 	const destJwtToken = await _fetchJwtToken(destSrvCred.url, destSrvCred.clientid, destSrvCred.clientsecret)
@@ -120,13 +145,13 @@ app.get('/podetails', async function (req, res) {
     try{
         const result =  await _poDetails(conSrvCred.onpremise_proxy_host, conSrvCred.onpremise_proxy_http_port, connJwtToken, destiConfi)
         
-        str = result.d.CreationDate;
-        res0 = str.split("cf(");
-        res1 = res0[1].split(")");   
-        res2 = new Date(parseInt(res1[0]));
-        result.d.CreationDate = res2;
+        // str = result.d.CreationDate;
+        // res0 = str.split("cf(");
+        // res1 = res0[1].split(")");   
+        // res2 = new Date(parseInt(res1[0]));
+        // result.d.CreationDate = res2;
 
-        res.json(result)
+        res.json(result);
     }
     catch(e) {
         console.log('Catch an error: ', e)
@@ -160,6 +185,118 @@ const _poDetails = async function(connProxyHost, connProxyPort, connJwtToken, de
         })
 	})    
 }
+
+app.get('/prdetails', async function (req, res) {
+    // call destination service
+	const destJwtToken = await _fetchJwtToken(destSrvCred.url, destSrvCred.clientid, destSrvCred.clientsecret)
+	const destiConfi = await _readDestinationConfig('s14_purchase_req', destSrvCred.uri, destJwtToken)
+    queryParam = url.parse(req.url, true).query;
+        
+    // call onPrem system via connectivity service and Cloud Connector
+	const connJwtToken = await _fetchJwtToken(conSrvCred.token_service_url, conSrvCred.clientid, conSrvCred.clientsecret)
+    try{
+        const result =  await _prDetails(conSrvCred.onpremise_proxy_host, conSrvCred.onpremise_proxy_http_port, connJwtToken, destiConfi)
+        
+        // str = result.d.vdatu;
+        // res0 = str.split("(");
+        // res1 = res0[1].split(")");   
+        // res2 = new Date(parseInt(res1[0]));
+        // result.d.vdatu = res2;
+        
+        // str = result.d.erdat;
+        // res0 = str.split("(");
+        // res1 = res0[1].split(")");
+        // res2 = new Date(parseInt(res1[0]));
+        // result.d.erdat = res2;
+
+        // console.log(result);
+        res.json(result);
+    }
+    catch(e) {
+        console.log('Catch an error: ', e)
+        res.json({"d":{"error": "error"}})
+    }
+})
+
+const _prDetails = async function(connProxyHost, connProxyPort, connJwtToken, destiConfi){
+    return new Promise((resolve, reject) => {
+        // console.log(q.number);
+        const targetUrl = destiConfi.URL + "/C_PurchaseReqnHeader(PurchaseRequisition='" + queryParam.number + "',DraftUUID=guid'00000000-0000-0000-0000-000000000000',IsActiveEntity=true)"
+        const encodedUser = Buffer.from(destiConfi.User + ':' + destiConfi.Password).toString("base64")
+    
+        const config = {
+            headers: {
+                Authorization: "Basic " + encodedUser,
+                'Proxy-Authorization': 'Bearer ' + connJwtToken,
+                'SAP-Connectivity-SCC-Location_ID': destiConfi.CloudConnectorLocationId        
+            },
+            proxy: {
+				host: connProxyHost, 
+				port: connProxyPort 
+            }              
+        }
+		axios.get(targetUrl, config)
+        .then(response => {
+           resolve(response.data)
+        })
+        .catch(error => {
+	      reject(error)
+        })
+	})    
+}
+
+app.get('/polist', async function (req, res) {
+    // call destination service
+	const destJwtToken = await _fetchJwtToken(destSrvCred.url, destSrvCred.clientid, destSrvCred.clientsecret)
+	const destiConfi = await _readDestinationConfig('s14_purchase_order', destSrvCred.uri, destJwtToken)
+    queryParam = url.parse(req.url, true).query;
+        
+    //call onPrem system via connectivity service and Cloud Connector
+	const connJwtToken = await _fetchJwtToken(conSrvCred.token_service_url, conSrvCred.clientid, conSrvCred.clientsecret)
+    try{
+        const result =  await _poList(conSrvCred.onpremise_proxy_host, conSrvCred.onpremise_proxy_http_port, connJwtToken, destiConfi)
+        
+//        str = result.d.CreationDate;
+//        res0 = str.split("cf(");
+//        res1 = res0[1].split(")");   
+//        res2 = new Date(parseInt(res1[0]));
+//        result.d.CreationDate = res2;
+
+        res.json(result)
+    }
+    catch(e) {
+        console.log('Catch an error: ', e)
+        res.json({"d":{"error": "error"}})
+    }
+})
+
+const _poList = async function(connProxyHost, connProxyPort, connJwtToken, destiConfi){
+    return new Promise((resolve, reject) => {
+        // console.log(q.number);
+        const targetUrl = destiConfi.URL + "/C_PurchaseOrderTP?sap-client=100&$skip=0&$top=25&$orderby=PurchaseOrder%20desc&$filter=IsActiveEntity%20eq%20false%20or%20SiblingEntity/IsActiveEntity%20eq%20null&$select=PurchaseOrder"
+        const encodedUser = Buffer.from(destiConfi.User + ':' + destiConfi.Password).toString("base64")
+    
+        const config = {
+            headers: {
+                Authorization: "Basic " + encodedUser,
+                'Proxy-Authorization': 'Bearer ' + connJwtToken,
+                'SAP-Connectivity-SCC-Location_ID': destiConfi.CloudConnectorLocationId        
+            },
+            proxy: {
+				host: connProxyHost, 
+				port: connProxyPort 
+            }              
+        }
+		axios.get(targetUrl, config)
+        .then(response => {
+           resolve(response.data)
+        })
+        .catch(error => {
+	      reject(error)
+        })
+	})    
+}
+
 
 // app.get('/sodetails', async function (req, res) {
 //     // call destination service
